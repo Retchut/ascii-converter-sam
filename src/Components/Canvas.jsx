6872f8c40213eaa929d1e28f919247e29be61825
Vespasiano <up201906834>
2023-05-02 11:44:28 +0100

    Change canvas add sample

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useRef } from "react";
-import LoadedIMG from "../Assets/sample.png";
+import LoadedIMG from "../Assets/sample2.png";
 
 function Canvas() {
   const canvasRef = useRef(null);
+  const imgRef = useRef(null);
 
   useEffect(() => {
     let canvas = canvasRef.current;
     let context = canvas.getContext("2d");
+    let img = imgRef.current;
+    context.drawImage(img, 0, 0);
   }, []);
 
   return (
     <div>
-      <img src={LoadedIMG} className="original-img" />
+      <img ref={imgRef} src={LoadedIMG} className="original-img" />
       <canvas ref={canvasRef} className="converted-canvas"></canvas>
     </div>
   );
