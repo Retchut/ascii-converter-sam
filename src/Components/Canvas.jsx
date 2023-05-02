543533623fb44c953e0b0c0a5b169bfeb8535def
Mário Travassos <retchut@protonmail.ch>
2023-05-02 12:15:40 +0100

    feat: get pixel values from canvas

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -1,21 +1,61 @@
 import { useEffect, useRef } from "react";
-import LoadedIMG from "../Assets/sample2.png";
+import LoadedIMG from "../Assets/sample.png";
+
+function getPixelData(canvasContext, x, y) {
+  return canvasContext.getImageData(x, y, 1, 1).data;
+}
 
 function Canvas() {
-  const canvasRef = useRef(null);
   const imgRef = useRef(null);
+  const srcRef = useRef(null);
+  const destRef = useRef(null);
+  let img;
+  let srcCanvas;
+  let destCanvas;
 
   useEffect(() => {
-    let canvas = canvasRef.current;
-    let context = canvas.getContext("2d");
-    let img = imgRef.current;
-    context.drawImage(img, 0, 0);
-  }, []);
+    img = imgRef.current;
+    /** @type {HTMLCanvasElement} */
+    srcCanvas = srcRef.current;
+    /** @type {HTMLCanvasElement} */
+    destCanvas = destRef.current;
+  }, [imgRef, srcRef, destRef]);
+
+  const setCanvas = () => {
+    // retrieve original image dimensions
+    const imgWidth = img.width;
+    const imgHeight = img.height;
+
+    // set canvas dimensions
+    srcCanvas.width = imgWidth;
+    srcCanvas.height = imgHeight;
+    destCanvas.width = imgWidth;
+    destCanvas.height = imgHeight;
+
+    // draw the image on the src canvas
+    const srcContext = srcCanvas.getContext("2d");
+    srcContext.drawImage(img, 0, 0);
+
+    //
+    console.log(getPixelData(srcContext, 10, 10));
+    console.log(getPixelData(srcContext, 180, 10));
+    console.log(getPixelData(srcContext, 10, 180));
+    console.log(getPixelData(srcContext, 180, 10));
+
+    const destContext = destCanvas.getContext("2d");
+    // draw on destination context canvas
+  };
 
   return (
-    <div>
-      <img ref={imgRef} src={LoadedIMG} className="original-img" />
-      <canvas ref={canvasRef} className="converted-canvas"></canvas>
+    <div className="canvas-container">
+      <img
+        ref={imgRef}
+        src={LoadedIMG}
+        onLoad={setCanvas}
+        className="src-img"
+      />
+      <canvas ref={srcRef} />
+      <canvas ref={destRef} />
     </div>
   );
 }
