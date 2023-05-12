63b6e6fd89785eceea6f24922af374c87a72faf9
Dimeu <diogop.2001@gmail.com>
2023-05-12 14:21:10 +0100

    base page display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div>popipopipopopipo</div>;
+
+  return <div className="page">
+    <h1 className="title">ASCII converter SAM</h1>
+    <input type="file"/>
+  </div>;
 }
 
 export default App;
